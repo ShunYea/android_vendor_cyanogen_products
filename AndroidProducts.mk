7c7703d08e2ecf6a516a6eee289e473188aa2337
ShunYea <shunyea@msn.com>
2012-01-04 16:56:34 +0800

    delete the motorola lge malata device

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,8 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_click.mk \
     $(LOCAL_DIR)/cyanogen_desirec.mk \
     $(LOCAL_DIR)/cyanogen_dream_sapphire.mk \
-    $(LOCAL_DIR)/cyanogen_droid2.mk \
-    $(LOCAL_DIR)/cyanogen_droid2we.mk \
     $(LOCAL_DIR)/cyanogen_encore.mk \
     $(LOCAL_DIR)/cyanogen_espresso.mk \
     $(LOCAL_DIR)/cyanogen_hero.mk \
@@ -22,21 +20,9 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_leo.mk \
     $(LOCAL_DIR)/cyanogen_liberty.mk \
     $(LOCAL_DIR)/cyanogen_mecha.mk \
-    $(LOCAL_DIR)/cyanogen_morrison.mk \
-    $(LOCAL_DIR)/cyanogen_motus.mk \
     $(LOCAL_DIR)/cyanogen_one.mk \
-    $(LOCAL_DIR)/cyanogen_olympus.mk \
-    $(LOCAL_DIR)/cyanogen_p970.mk \
-    $(LOCAL_DIR)/cyanogen_p990.mk \
-    $(LOCAL_DIR)/cyanogen_p999.mk \
     $(LOCAL_DIR)/cyanogen_passion.mk \
     $(LOCAL_DIR)/cyanogen_saga.mk \
-    $(LOCAL_DIR)/cyanogen_shadow.mk \
-    $(LOCAL_DIR)/cyanogen_sholes.mk \
-    $(LOCAL_DIR)/cyanogen_smb_a1002.mk \
-    $(LOCAL_DIR)/cyanogen_smb_a1004.mk \
-    $(LOCAL_DIR)/cyanogen_smb_a1011.mk \
-    $(LOCAL_DIR)/cyanogen_smb_b9701.mk \
     $(LOCAL_DIR)/cyanogen_speedy.mk \
     $(LOCAL_DIR)/cyanogen_supersonic.mk \
     $(LOCAL_DIR)/cyanogen_u8220.mk \
@@ -46,5 +32,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_vivo.mk \
     $(LOCAL_DIR)/cyanogen_vivow.mk \
     $(LOCAL_DIR)/cyanogen_z71.mk \
-    $(LOCAL_DIR)/cyanogen_zeppelin.mk \
     $(LOCAL_DIR)/cyanogen_zero.mk
